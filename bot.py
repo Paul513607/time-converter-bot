b7e7a8d1cbef7a0fb5a0ecb4194ed6d05279b5e5
Paul Samson <79131709+Paul513607@users.noreply.github.com>
2025-03-16 20:52:24 +0200

    update slash commands, docker, cleanup

--- bot.py
+++ bot.py
@@ -1,24 +1,24 @@
+import traceback
 import discord
 from discord.ext import commands
 from discord import app_commands
 import dotenv
 import os
-from convert.converter import Converter
 from database.connection import Connection
+from convert import converter_utils
 
 dotenv.load_dotenv()
 token = os.getenv('TOKEN')
 
 class Client(commands.Bot):
-    message_converter: Converter
     db_connection: Connection
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
 
     async def on_ready(self):
+
         try:
-            self.message_converter = Converter()
             self.db_connection = Connection(
                 os.getenv('DB_NAME'),
                 os.getenv('DB_USER'),
@@ -26,12 +26,8 @@ class Client(commands.Bot):
                 os.getenv('DB_HOST'),
                 os.getenv('DB_PORT')
             )
-
-            guild = discord.Object(id=os.getenv('GUILD_ID'))
-            synced = await self.tree.sync(guild=guild)
-            print(f"Synced {len(synced)} commands to {guild.id}")
         except Exception as e:
-            print(f"Error initializing bot: {e}")
+            traceback.print_exc()
             return
 
         print(f'Logged on as {self.user}!')
@@ -43,7 +39,7 @@ class Client(commands.Bot):
         user_timezone_str = self.db_connection.get_user(message.author.id)[1]
         if user_timezone_str is None:
             return
-        timestamp = self.message_converter.parse_message(message.content, user_timezone_str)
+        timestamp = converter_utils.parse_message(message.content, user_timezone_str)
         if timestamp != -1:
             self.db_connection.insert_message(message.id, timestamp)
             await message.add_reaction('🕒')
@@ -59,7 +55,7 @@ class Client(commands.Bot):
                 return
             
             timestamp = message[1]
-            tag = self.message_converter.convert_to_tag(timestamp)
+            tag = converter_utils.convert_to_tag(timestamp)
             await reaction.message.channel.send(tag)
 
         
@@ -67,11 +63,9 @@ intents = discord.Intents.default()
 intents.message_content = True
 client = Client(command_prefix='!', intents=intents)
 
-GUILD_ID = discord.Object(id=os.getenv('GUILD_ID'))
-
-@client.tree.command(name='set-me', description='Set your timezone', guilds=[GUILD_ID])
+@client.tree.command(name='set-me', description='Set your timezone')
 async def set_me(interaction: discord.Interaction, timezone: str):
-    if (not client.message_converter.verify_timezone(timezone)):
+    if (not converter_utils.verify_timezone(timezone)):
         await interaction.response.send_message('Invalid timezone format. Please use UTC+/-X', ephemeral=True)
         return
     if (client.db_connection.get_user(interaction.user.id) is not None):
@@ -79,9 +73,10 @@ async def set_me(interaction: discord.Interaction, timezone: str):
     client.db_connection.insert_user(interaction.user.id, timezone)
     await interaction.response.send_message(f'Timezone set to {timezone}', ephemeral=True)
 
-@client.tree.command(name='remove-me', description='Remove your timezone', guilds=[GUILD_ID])
+@client.tree.command(name='remove-me', description='Remove your timezone')
 async def remove_me(interaction: discord.Interaction):
     client.db_connection.delete_user(interaction.user.id)
     await interaction.response.send_message('Timezone removed', ephemeral=True)
 
+print("Starting bot")
 client.run(token)
